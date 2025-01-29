59d9b561dad59466cbd0d296ba07172840f5d4f6
mysterytyy <wellchanmaquiso47@gmail.com>
2025-01-29 09:30:37 +0800

    latest update

---
+++ resources/js/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-app>
+      <ExampleComponent />
+    </v-app>
+  </template>
+
+  <script>
+  import ExampleComponent from './components/ExampleComponent.vue'
+
+  export default {
+    name: 'App',
+    components: {
+      ExampleComponent,
+    },
+  }
+  </script>
---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-card>
+            <v-card-title>Hello, Vuetify 3!</v-card-title>
+            <v-card-text>
+              This is a simple example of using Vuetify 3 with Laravel and Vue.js.
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </template>
+
+  <script>
+  export default {
+    name: 'ExampleComponent',
+  }
+  </script>
